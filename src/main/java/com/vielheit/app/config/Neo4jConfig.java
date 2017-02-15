5df487d2a1abb3674b5f42493c93304313c0becb
jmcelwain <jmcelwain@gmail.com>
2017-02-15 00:48:26 -0600

    upgrade SND to fix session persist bug

--- src/main/java/com/vielheit/app/config/Neo4jConfig.java
+++ src/main/java/com/vielheit/app/config/Neo4jConfig.java
@@ -4,8 +4,8 @@ import org.neo4j.ogm.session.SessionFactory;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.neo4j.config.Neo4jConfiguration;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.data.neo4j.transaction.Neo4jTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -14,19 +14,17 @@ import javax.sql.DataSource;
 
 @Configuration
 @EnableTransactionManagement
-@EnableNeo4jRepositories(
-        basePackages = "com.vielheit.app.repository")
-public class Neo4jConfig extends Neo4jConfiguration {
+@EnableNeo4jRepositories(basePackages = "com.vielheit.app.repository")
+@ComponentScan({"com.vielheit.app"})
+public class Neo4jConfig {
     @Bean
-    @Override
     public SessionFactory getSessionFactory() {
         return new SessionFactory("com.vielheit.app.domain");
     }
 
     @Bean(name="graphTransactionManager")
-    @Override
     public Neo4jTransactionManager transactionManager() {
-        return new Neo4jTransactionManager(getSessionFactory().openSession());
+        return new Neo4jTransactionManager(getSessionFactory());
     }
 
     @Bean
