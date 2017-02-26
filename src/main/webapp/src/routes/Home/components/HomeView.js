b7c97222d87f04165df530d8059517318f540590
James McElwain <jmcelwain@gmail.com>
2017-02-25 22:33:20 -0600

    remove yarn lock

--- src/main/webapp/src/routes/Home/components/HomeView.js
+++ src/main/webapp/src/routes/Home/components/HomeView.js
@@ -5,11 +5,10 @@ import SideMenu from '../../../components/Menu/Menu'
 export const HomeView = () => (
   <div>
     <div style={{ float: 'right', borderBottom: '1px dashed black' }}>
-      <i><b>the animal is in the world like water in water. ... that is essentially on a level with the world in which it moves like water in water</b></i>
+      <i><b>the animal is in the world like water in water</b></i>
     </div>
 
     <SideMenu style={{ float: 'left' }} />
   </div>
 )
-
 export default HomeView
