687e9c6d97e1989b7d3b1f3ec9a65c99485aea2d
James McElwain <jmcelwain@gmail.com>
2017-04-01 14:40:21 -0500

    Fix roles

--- src/main/java/com/vielheit/core/domain/Role.java
+++ src/main/java/com/vielheit/core/domain/Role.java
@@ -1,7 +1,7 @@
 package com.vielheit.core.domain;
 
 public enum Role {
-    ADMIN, USER();
+    REFRESH_TOKEN, ADMIN, USER;
 
     public String authority() {
         return "ROLE_" + this.name();
