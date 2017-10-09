27fc9bea978f0bea4c40fc960b43e9c2cd2010fe
James McElwain <jmcelwain@gmail.com>
2017-10-08 19:43:31 -0500

    stub out editor state

--- src/main/webapp/src/routes/Editor/components/Editor.tsx
+++ src/main/webapp/src/routes/Editor/components/Editor.tsx
@@ -5,25 +5,47 @@ import { EditorProps } from '../containers/EditorContainer'
 import EditorMenu from './EditorMenu'
 import AbstractionTypeEditor from './AbstractionTypeEditor'
 
-export const Editor = (props: EditorProps) => {
-  const selectedItem = props.editorState.selectedItem + ""
 
-  return (
-    <div className="editor-container">
-      <Layout>
-        <Layout.Sider width="0%">
-          <EditorMenu/>
-        </Layout.Sider>
-        <Layout.Content>
-          <AbstractionTypeEditor
-            submitAbstractionType={props.submitAbstractionType}
-            types={props.editorState.types}
-            editorState={props.editorState}
-            editor={props.editor}/>
-        </Layout.Content>
-      </Layout>
-    </div>
-  )
+export interface EditorState {
+  editor: () => JSX.Element
+}
+
+export class Editor extends React.Component<EditorProps, EditorState> {
+  private AbstractionType = () => <AbstractionTypeEditor
+      submitAbstractionType={this.props.submitAbstractionType}
+      types={this.props.editorState.types}
+      editorState={this.props.editorState}
+      editor={this.props.editor}/>
+  private Abstraction = () => <div>STUB ABSTRACTION</div>
+  private Entry = () => <div>STUB ENTRY</div>
+
+  public state = {
+    editor: this.AbstractionType
+  }
+
+  public selectEditor(editor: 'Abstraction' | 'AbstractionType' | 'Entry') {
+    this.setState({
+      editor: this[editor]
+    })
+  }
+
+  public render() {
+    const selectedItem = this.props.editorState.selectedItem + ""
+    const Editor = this.state.editor
+
+    return (
+      <div className="editor-container">
+        <Layout>
+          <Layout.Sider width="0%">
+            <EditorMenu selectEditor={this.selectEditor.bind(this)}/>
+          </Layout.Sider>
+          <Layout.Content>
+            <Editor/>
+          </Layout.Content>
+        </Layout>
+      </div>
+    )
+  }
 }
 
 export default Editor
--- src/main/webapp/src/routes/Editor/components/EditorMenu.tsx
+++ src/main/webapp/src/routes/Editor/components/EditorMenu.tsx
@@ -4,22 +4,34 @@ import { Menu, Icon } from 'antd'
 const Item = Menu.Item
 const SubMenu = Menu.SubMenu
 
-export default () => (<Menu
-  defaultSelectedKeys={['1']}
-  defaultOpenKeys={['sub1']}
-  mode="inline"
-  theme="dark"
-  inlineCollapsed={true}>
-  <Menu.Item key="1">
-    <Icon type="book" />
-    <span>Entries</span>
-  </Menu.Item>
-  <Menu.Item key="2">
-    <Icon type="cloud" />
-    <span>Abstractions</span>
-  </Menu.Item>
-  <Menu.Item key="3">
-    <Icon type="share-alt" />
-    <span>Types</span>
-  </Menu.Item>
-</Menu>)
+export interface EditorMenuProps {
+  selectEditor(name: string): void
+}
+
+export default class EditorMenu extends React.Component<EditorMenuProps, {}> {
+  public render() {
+    return (
+      <Menu
+        onSelect={({ key }) => this.props.selectEditor(key)}
+        defaultSelectedKeys={['1']}
+        defaultOpenKeys={['sub1']}
+        mode="inline"
+        theme="dark"
+        inlineCollapsed={true}>
+        <Menu.Item key="Entry">
+          <Icon type="book"/>
+          <span>Entries</span>
+        </Menu.Item>
+        <Menu.Item key="Abstraction">
+          <Icon type="share-alt"/>
+          <span>Abstractions</span>
+        </Menu.Item>
+        <Menu.Item key="AbstractionType">
+          <Icon type="cloud"/>
+          <span>Types</span>
+        </Menu.Item>
+      </Menu>
+    )
+  }
+}
+
