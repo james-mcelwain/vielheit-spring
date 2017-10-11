a778517eab751539b727279795e486a973ad6a7b
James McElwain <jmcelwain@gmail.com>
2017-10-10 20:02:39 -0500

    Add stop script, add start systemd units to start scripts

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-(cd ./src/main/webapp && yarn start)
+sudo systemctl start redis
+sudo systemctl start postgresql
+sudo systemctl start neo4j
+(cd ./src/main/webapp && npm start)
---
+++ stop.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+sudo systemctl stop redis
+sudo systemctl stop postgresql
+sudo systemctl stop neo4j
+
